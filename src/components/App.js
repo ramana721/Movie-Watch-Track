b15ead0c7dd3730d44e12ce3ae95a3cca0cd9247
ramana721 <venkataramana7210@gmail.com>
2025-06-06 14:13:41 +0530

    Final Changes

--- src/components/App.js
+++ src/components/App.js
@@ -11,28 +11,6 @@ import MovieDetails from "./main/MovieDetails";
 // import axios from 'axios';
 
 
-const tempWatchedData = [
-  {
-    imdbID: "tt1375666",
-    Title: "Inception",
-    Year: "2010",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-    runtime: 148,
-    imdbRating: 8.8,
-    userRating: 10,
-  },
-  {
-    imdbID: "tt0088763",
-    Title: "Back to the Future",
-    Year: "1985",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
-    runtime: 116,
-    imdbRating: 8.5,
-    userRating: 9,
-  },
-];
 
 export const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
@@ -41,9 +19,9 @@ const apikey = "a1af4c5a";
 // www.omdbapi.com/?s=${query}&apikey=${apikey}
 
 export default function App() {
-  const [query, setQuery] = useState("real");
+  const [query, setQuery] = useState("");
   const [movies, setMovies] = useState([]);
-  const [watched, setWatched] = useState(tempWatchedData);
+  const [watched, setWatched] = useState([]);
   const [isOpen1, setIsOpen1] = useState(true);
   const [isOpen2, setIsOpen2] = useState(true);
   const [isLoading1, setIsLoading1] = useState(false);
